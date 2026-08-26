3842a5f51c7197395d4918cb412639a097332046
End-Mohammed90 <abed09v@gmail.com>
2026-08-26 11:58:17 +0300

    Bugfix: SyntaxError: Identifier 'updateInterests' has already been declared

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,7 +11,6 @@ const {
   changePassword,
   updateInterests,
   logout,
-  updateInterests
 } = require('../controllers/authController');
 
 const uploadMiddleware = require('../middlewares/uploadMiddleware');
